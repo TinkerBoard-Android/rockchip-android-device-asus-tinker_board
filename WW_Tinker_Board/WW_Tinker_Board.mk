3284b5451d25fe2f0b68754d09d7bf35ee70f641
Leslie Yu <leslie_yu@asus.com>
2021-11-18 16:30:11 +0800

    Add the device WW_Tinker_Board
    The folder device/asus/tinker_board is copied from the folder device/rockchip/rk3288.
    
    The following file is not copied since this is for a device which is not used.
    rk3288.mk
    
    The following folders are not copied since we don’t use them yet.
    preinstall/
    preinstall_del/
    preinstall_del_forever/
    
    The following files are not copied and we use them from the folder device/rockchip/rk3288 first.
    package_performance.xml
    media_profiles_default.xml
    external_camera_config.xml
    
    The following folder is not copied since the module is defined in the folder device/rockchip/rk3288/verity as well and we don’t use it yet.
    verity/
    
    The following items are copied but not used yet.
    fstab.rk30board
    manifest.xml
    manifest_ab.xml
    sepolicy/
    wifi_bt.mk
    
    The following folder device/asus/tinker_board/WW_Tinker_Board is copied from device/rockchip/rk3288/rk3288_Android11.
    
    The following folders are not copied since we don’t use them yet.
    preinstall/
    preinstall_del/
    preinstall_del_forever/
    
    Change-Id: I8ebe8b982dc0bf4e15960345ad9a3addaadd9d54

---
+++ WW_Tinker_Board/WW_Tinker_Board.mk
@@ -0,0 +1,43 @@
+#
+# Copyright 2014 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# First lunching is R, api_level is 30
+PRODUCT_SHIPPING_API_LEVEL := 30
+PRODUCT_DTBO_TEMPLATE := $(LOCAL_PATH)/dt-overlay.in
+PRODUCT_BOOT_DEVICE := ff0f0000.dwmmc
+PRODUCT_SDMMC_DEVICE := ff0c0000.dwmmc
+include device/rockchip/common/build/rockchip/DynamicPartitions.mk
+include device/asus/tinker_board/WW_Tinker_Board/BoardConfig.mk
+include device/rockchip/common/BoardConfig.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/asus/tinker_board/device.mk)
+$(call inherit-product, device/rockchip/common/device.mk)
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/../overlay
+
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_NAME := WW_Tinker_Board
+PRODUCT_DEVICE := WW_Tinker_Board
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := WW_Tinker_Board
+PRODUCT_MANUFACTURER := asus
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+#
+## add Rockchip properties
+#
+PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=280
