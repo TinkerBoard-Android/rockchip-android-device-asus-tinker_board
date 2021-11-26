875b494f975863a84721da645f9e49bb1c175499
yupei_lin <yupei_lin@asus.com>
2021-11-26 16:06:11 +0800

    Audio : add persist to support SPDIF function.
    Change-Id: I468d441e9322b3b1449055abede84f6cf570218e

--- device.mk
+++ device.mk
@@ -177,4 +177,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
                 ro.rk.displayd.enable=false \
                 vendor.hwc.device.primary=HDMI-A \
                 vendor.gralloc.no_afbc_for_fb_target_layer=1 \
-		vendor.gralloc.no_afbc_for_sf_client_layer=1
+                vendor.gralloc.no_afbc_for_sf_client_layer=1 \
+                persist.spdif_sounds=0
