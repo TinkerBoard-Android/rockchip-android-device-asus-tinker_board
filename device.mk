399637f55ef2d7092975d71b028451e5fd80f351
tzuwen_chang <TzuWen_Chang@asus.com>
2022-05-04 15:22:58 +0800

    add properties for tcp adb
    Change-Id: I3552ef39d0e935ef4af567bc040d0a4460ccfefe

--- device.mk
+++ device.mk
@@ -201,6 +201,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
                 vendor.gralloc.no_afbc_for_sf_client_layer=1 \
                 persist.spdif_sounds=0
 
+#add properties for tcp adb
+PRODUCT_PROPERTY_OVERRIDES += persist.adb.tcp.port = 5555
+
 PRODUCT_COPY_FILES += \
     vendor/rockchip/common/bin/$(TARGET_ARCH)/e2fsck:recovery/root/sbin/e2fsck \
     vendor/rockchip/common/bin/$(TARGET_ARCH)/resize2fs:recovery/root/sbin/resize2fs \
